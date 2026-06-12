b975db88984b18ab0cc3063d22f388f4bbc90954
KaZe <kaze.pro@hotmail.com>
2026-06-12 14:46:55 +0200

    Create ExplosiveBowListener.java

---
+++ src/main/java/fr/tonnom/tntbow/ExplosiveBowListener.java
@@ -0,0 +1,36 @@
+package fr.tonnom.tntbow;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class ExplosiveBowListener implements Listener {
+
+    @EventHandler
+    public void onArrowHit(ProjectileHitEvent event) {
+        if (event.getEntity() instanceof Arrow) {
+            Arrow arrow = (Arrow) event.getEntity();
+
+            if (arrow.getShooter() instanceof Player) {
+                Player player = (Player) arrow.getShooter();
+                ItemStack bow = player.getInventory().getItemInMainHand();
+
+                if (bow.hasItemMeta() && "§cArc TNT".equals(bow.getItemMeta().getDisplayName())) {
+                    Location hitLocation = arrow.getLocation();
+                    World world = hitLocation.getWorld();
+
+                    if (world != null) {
+                        // Force de 7.0F pour un trou énorme (La TNT de base est à 4.0F)
+                        world.createExplosion(hitLocation, 7.0F, true, true, player);
+                    }
+                    arrow.remove();
+                }
+            }
+        }
+    }
+}
