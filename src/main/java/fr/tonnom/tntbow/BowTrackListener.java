ab3c13e6290fe0c07f584c620efaecf39fd82ed9
KaZe <kaze.pro@hotmail.com>
2026-06-13 14:02:29 +0200

    Update BowTrackListener.java

--- src/main/java/fr/tonnom/tntbow/BowTrackListener.java
+++ src/main/java/fr/tonnom/tntbow/BowTrackListener.java
@@ -1,13 +1,9 @@
 package fr.tonnom.tntbow;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.ItemDespawnEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class BowTrackListener implements Listener {
@@ -18,16 +14,18 @@ public class BowTrackListener implements Listener {
         this.plugin = plugin;
     }
 
-    // Quand un joueur meurt : l'arc drop normalement, rien à faire
-    // Mais on surveille si l'arc tombe dans le vide (ItemDespawnEvent avec y < 0)
     @EventHandler
     public void onItemDespawn(ItemDespawnEvent event) {
         ItemStack item = event.getEntity().getItemStack();
         if (Main.isTNTBow(item)) {
-            // L'item despawn = soit tombé dans le vide, soit temps écoulé
             if (event.getEntity().getLocation().getY() < 0) {
+                String owner = Main.getBowOwner();
                 Main.setBowExists(false);
-                Bukkit.broadcastMessage("§4§l[TNTBow] §cL'Arc TNT est tombé dans le vide... Il est perdu à tout jamais et ne peut plus être utilisé !");
+                Main.setBowOwner(null);
+                Bukkit.broadcastMessage("§4§l[TNTBow] §cL'Arc TNT" 
+                    + (owner != null ? " de §6" + owner + " §c" : " ")
+                    + "est tombé dans le vide... Il est perdu à tout jamais et ne peut plus être utilisé !");
+                plugin.getLogger().info("[TNTBow] Arc TNT perdu dans le vide. Ancien propriétaire : " + (owner != null ? owner : "inconnu"));
             }
         }
     }
